fb4562fb0bc7aa4df6efb1dad02ff12ddfc2cdd6
Murad Popattia <muradmansoor189@gmail.com>
2021-11-16 19:28:36 +0500

    Added a python concept notebook and modified image captioning code

--- PyTorch Work/Image Captioning Model/model.py
+++ PyTorch Work/Image Captioning Model/model.py
@@ -3,4 +3,23 @@ import torch.nn as nn
 import torchvision.models as models
 
 class EncoderCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, embed_size, trainCNN=False):
+        super(EncoderCNN, self).__init__()
+        self.trainCNN = trainCNN
+        self.inception = models.inception_v3(pretrained=True, aux_logits=False) # aux_logits only used for loss during training
+
+        # (fc): Linear(in_features=2048, out_features=1000, bias=True)
+        # we replace the FC layer with our own layer
+        self.inception.fc = nn.Linear(self.inception.fc.in_features, embed_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self):
+        pass
+
+
+if __name__ == '__main__':
+    inception = models.inception_v3(pretrained=False)
+
+    # (fc): Linear(in_features=2048, out_features=1000, bias=True)
+    print(inception)
